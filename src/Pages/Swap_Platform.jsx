d90efa9c8c8048ca70508c574d37ffbd71ce9685
Ashi Jain <ashi.jain.1411@gmail.com>
2025-07-12 12:54:22 +0530

    Initial commit

---
+++ src/Pages/Swap_Platform.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Swap_Platform() {
+  return (
+    <div>Swap_Platform</div>
+  )
+}
---
+++ src/Pages/Swap_Requests.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Swap_Requests() {
+  return (
+    <div>Swap_Requests</div>
+  )
+}
